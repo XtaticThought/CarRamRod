44007120d1adf3cb87f110e9e848d8ec516cdc10
Xtatic Thought Inc <xtaticthought@gmail.com>
2024-01-05 11:38:01 -0500

    Initial Commit

---
+++ Floor Is LavaUITests/Floor_Is_LavaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Floor_Is_LavaUITestsLaunchTests.swift
+//  Floor Is LavaUITests
+//
+//  Created by Leona Shimoru on 1/5/24.
+//
+
+import XCTest
+
+final class Floor_Is_LavaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
